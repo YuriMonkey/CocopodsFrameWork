011f85621124b1b70c50595bef1280bcfe61d8f3
海棠 侯 <280431977@qq.com>
2019-07-03 15:57:50 +0800

    no message

---
+++ CocoTestFrameWork.framework/Headers/CocoTestFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  CocoTestFrameWork.h
+//  CocoTestFrameWork
+//
+//  Created by jimi on 2019/7/1.
+//  Copyright © 2019 jimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoTestFrameWork.
+FOUNDATION_EXPORT double CocoTestFrameWorkVersionNumber;
+
+//! Project version string for CocoTestFrameWork.
+FOUNDATION_EXPORT const unsigned char CocoTestFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoTestFrameWork/PublicHeader.h>
+#import "Test.h"
+
---
+++ CocoTestFrameWork.framework/Headers/Test.h
@@ -0,0 +1,19 @@
+//
+//  Test.h
+//  CocoTestFrameWork
+//
+//  Created by jimi on 2019/7/1.
+//  Copyright © 2019 jimi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+- (void) sayHello;
+
+@end
+
+NS_ASSUME_NONNULL_END
